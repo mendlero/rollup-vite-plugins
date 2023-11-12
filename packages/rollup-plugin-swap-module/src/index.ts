5225c986385fea3e19afaa1f75b5e441422f9ddc
Omer M <omerplays1411@gmail.com>
2023-11-12 18:05:08 +0200

    uses resolveId instead of load

--- packages/rollup-plugin-swap-module/src/index.ts
+++ packages/rollup-plugin-swap-module/src/index.ts
@@ -3,8 +3,8 @@ import { Plugin } from 'vite';
 export default function swapModule(module1: string, module2: string): Plugin {
   return {
     name: `Swap ${module1} with ${module2}`,
-    load(id, options) {
-      if (id == module1) {
+    resolveId(source, importer, options) {
+      if (source == module1) {
         return module2;
       }
 
