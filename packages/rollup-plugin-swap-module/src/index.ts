bc7496c2a1d0e45212879319b563a30a98e7f8a5
Omer M <omerplays1411@gmail.com>
2023-11-22 18:41:30 +0200

    removed unneccessary paramaters from resolveId

--- packages/rollup-plugin-swap-module/src/index.ts
+++ packages/rollup-plugin-swap-module/src/index.ts
@@ -3,8 +3,8 @@ import { Plugin } from 'vite';
 export default function swapModule(module1: string, module2: string): Plugin {
   return {
     name: `Swap ${module1} with ${module2}`,
-    resolveId(source, importer, options) {
-      if (source == module1) {
+    resolveId(id) {
+      if (id == module1) {
         return module2;
       }
 
