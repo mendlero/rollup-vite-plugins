4b7260de795f1ee9ddabf0bca4a72f0e0bd9da1d
Omer M <omerplays1411@gmail.com>
2023-11-23 13:42:52 +0200

    now eporting only the reactPlugin

--- packages/vite-plugin-react-plus-vue/src/index.ts
+++ packages/vite-plugin-react-plus-vue/src/index.ts
@@ -1,18 +1,18 @@
-import { Plugin } from 'vite';
 import reactPlugin, {
   Options as reactPluginOptions,
 } from '@vitejs/plugin-react';
 
-export interface Options {
-  reactPluginSettings?: Omit<reactPluginOptions, 'jsxImportSource'>;
-}
+export type Options = Omit<
+  reactPluginOptions,
+  'jsxImportSource' | 'jsxRuntime'
+>;
 
 export default function reactPlusVue(
   options: Options,
-): Plugin | [ReturnType<typeof reactPlugin>, Plugin] {
-  const reactVuePlugin: Plugin = {
-    name: 'React plus vue',
-  };
-
-  return [reactPlugin(options.reactPluginSettings), reactVuePlugin];
+): ReturnType<typeof reactPlugin> {
+  return reactPlugin({
+    ...options,
+    jsxImportSource: 'vite-plugin-react-plus-vue/jsxImportSource',
+    jsxRuntime: 'automatic',
+  });
 }
